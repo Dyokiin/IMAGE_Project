e28d842a6e7ec5f402b441bdf46bdb9d321d9ada
Dyokiin <tarnauld@etud.u-pem.fr>
2021-04-28 12:58:13 +0200

    QuadTree Basic Skeleton

---
+++ include/QuadTree.h
@@ -0,0 +1,43 @@
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+
+typedef struct QTNodeData {
+    float x;
+    float y;
+    float height;
+} QTNodeData;
+
+QTNodeData QTNodeDataMake(float x, float y, float height);
+
+typedef struct QTCorners {
+    float x1;
+    float x2;
+    float y1;
+    float y2;
+} QTCorners;
+
+QTCorners QTCornersMake(float x1, float x2, float y1, float y2);
+
+class QTNode { 
+protected:    
+    QTNode *nW;
+    QTNode *nE;
+    QTNode *sW;
+    QTNode *sE;
+    QTCorners area;
+    QTNodeData data;
+public:
+    QTNode(QTCorners area);
+    ~QTNode();
+
+    QTNodeData getData();
+    QTCorners getArea();
+};
+
+class QTree : public QTNode {
+public:
+    QTree();
+};
+
+#endif
---
+++ src/QuadTree.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "../include/QuadTree.h"
+
+
+QTNodeData QTNodeDataMake(float x, float y, float height){
+
+    QTNodeData data;
+    data.x = x;
+    data.y = y;
+    data.height = height;
+
+    return data;
+}
+
+QTCorners QTCornersMake(float x1, float x2, float y1, float y2){
+
+    QTCorners corners;
+    corners.x1 = x1;
+    corners.x2 = x2;
+    corners.y1 = y1;
+    corners.y2 = y2;
+
+    return corners;
+}
+
+QTNode::QTNode(QTCorners corners){
+
+    this->nW = NULL;
+    this->nE = NULL;
+    this->sE = NULL;
+    this->sW = NULL;
+    this->area = corners;
+    this->data = (QTNodeData*)malloc(sizeof(QTNodeData));
+}
+
+QTNode::~QTNode(){
+    if(this->nE!=NULL) delete this->nE;
+    if(this->nW!=NULL) delete this->nW;
+    if(this->sE!=NULL) delete this->sE;
+    if(this->sW!=NULL) delete this->sW;
+
+    free(this->data);
+    
+    delete this->data;
+}
+
+QTree::QTree(){
+    //tbd
+}
