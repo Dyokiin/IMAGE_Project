ca30ea3b0a462730f9fdf325f8d7baef77375503
Dyokiin <tarnauld@etud.u-pem.fr>
2021-05-21 19:02:24 +0200

    y

---
+++ include/AffTxt.h
@@ -0,0 +1,13 @@
+
+#ifndef AFFTXT_H
+#define AFFTXT_H
+#include <SDL2/SDL.h>
+
+
+void AffL(int n, float taille, int x, int y, SDL_Renderer* ren);
+int AffTxt(std::string mot, float taille, int x, int y, SDL_Renderer* ren);
+void AffChfr(int chiffre, float taille, int x, int y, SDL_Renderer* ren);
+int AffNb(int nb, float taille, int x, int y, SDL_Renderer* ren);
+void DrawBot(int x, int y, SDL_Renderer* ren);
+
+#endif 
